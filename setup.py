ea876c304e51fbf6edc442ec4ee0a541cf412567
Nathan Wilcox <nejucomo@gmail.com>
2016-04-25 17:08:17 -0700

    Augment ./setup.py to implement a consistent release process.

--- setup.py
+++ setup.py
@@ -1,28 +1,90 @@
 #! /usr/bin/env python
 
-from setuptools import setup, find_packages
+import sys
+import subprocess
+import setuptools
 
 
-setup(
-    name='onslaught',
-    version='0.1.dev1',
-    author='Nathan Wilcox',
-    author_email='nejucomo@gmail.com',
-    license='GPLv3',
-    url='https://github.com/nejucomo/onslaught',
+PACKAGE = 'onslaught'
+CODE_SIGNING_GPG_ID = '29F306D804101C610BDEA41F5F53C65730693904'
 
-    packages=find_packages(),
 
-    entry_points={
-        'console_scripts': [
-            'onslaught = onslaught.main:main',
-            'onslaught-check-sdist-log = onslaught.check_sdist_log:main',
+def setup():
+    setuptools.setup(
+        name=PACKAGE,
+        version='0.1.dev1',
+        author='Nathan Wilcox',
+        author_email='nejucomo@gmail.com',
+        license='GPLv3',
+        url='https://github.com/nejucomo/{}'.format(PACKAGE),
+
+        packages=setuptools.find_packages(),
+
+        entry_points={
+            'console_scripts': [
+                '{0} = {0}.main:main'.format(PACKAGE),
+                ('{0}-check-sdist-log = {0}.check_sdist_log:main'
+                 .format(PACKAGE)),
             ],
         },
 
-    install_requires=[
-        'flake8 >= 2.0',
-        'coverage >= 3.6',
-        'virtualenv >= 13.1.2',
+        install_requires=[
+            'flake8 >= 2.0',
+            'coverage >= 3.6',
+            'virtualenv >= 13.1.2',
         ],
     )
+
+
+def main(args=sys.argv[1:]):
+    if args == ['release']:
+        publish_release()
+    else:
+        setup()
+
+
+def publish_release():
+    # TODO: require self-onslaught to pass as policy.
+    # ref: https://github.com/nejucomo/onslaught/issues/8
+
+    def sh(*args):
+        print 'Running {!r}...'.format(args)
+        return subprocess.check_call(args)
+
+    def shout(*args):
+        print 'Running {!r}...'.format(args)
+        return subprocess.check_output(args)
+
+    gitstatus = shout('git', 'status', '--porcelain')
+    if gitstatus.strip():
+        raise SystemExit(
+            'ABORT: dirty working directory:\n{}'.format(
+                gitstatus,
+            )
+        )
+
+    branch = shout('git', 'rev-parse', '--abbrev-ref', 'HEAD').strip()
+    if branch != 'release':
+        raise SystemExit(
+            'ABORT: must be on release branch, not {!r}'.format(
+                branch,
+            ),
+        )
+
+    version = shout('python', './setup.py', '--version').strip()
+    print 'Creating git tag: {!r}'.format(version)
+    sh('git', 'tag', version)
+
+    print 'Uploading sdist...'
+    sh(
+        'python',
+        './setup.py',
+        'sdist',
+        'upload',
+        '--sign',
+        '--identity', CODE_SIGNING_GPG_ID,
+    )
+
+
+if __name__ == '__main__':
+    main()
